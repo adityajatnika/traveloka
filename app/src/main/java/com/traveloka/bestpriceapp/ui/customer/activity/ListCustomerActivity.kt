1ff5279345b576009818eed5009f3bafd358f642
adityajatnika <adityaadjatnika2@gmail.com>
2022-06-02 21:37:18 +0700

    add activity & architecture app

---
+++ app/src/main/java/com/traveloka/bestpriceapp/ui/customer/activity/ListCustomerActivity.kt
@@ -0,0 +1,12 @@
+package com.traveloka.bestpriceapp.ui.customer.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.traveloka.bestpriceapp.R
+
+class ListCustomerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_customer)
+    }
+}
--- app/src/main/java/com/traveloka/bestpriceapp/ui/produk/activity/MainActivity.kt
+++ app/src/main/java/com/traveloka/bestpriceapp/ui/produk/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.traveloka.bestpriceapp
+package com.traveloka.bestpriceapp.ui.produk.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.traveloka.bestpriceapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
