4fae9c1cef9ac3c3b3dc2e741bc29445b13baac5
adityajatnika <adityaadjatnika2@gmail.com>
2022-06-06 17:46:49 +0700

    pre pull

--- app/src/main/java/com/traveloka/bestpriceapp/ui/ui/home/HomeFragment.kt
+++ app/src/main/java/com/traveloka/bestpriceapp/ui/ui/home/HomeFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.traveloka.bestpriceapp.ui.databinding.FragmentHomeBinding
+import com.traveloka.bestpriceapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
--- app/src/main/java/com/traveloka/bestpriceapp/ui/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/traveloka/bestpriceapp/ui/ui/notifications/NotificationsFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.traveloka.bestpriceapp.ui.databinding.FragmentNotificationsBinding
+import com.traveloka.bestpriceapp.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
