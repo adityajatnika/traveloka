23ff6da1019a2a29e116e38c12a3fcb70952985e
Muhammad Fadhiil <muhammadff17@gmail.com>
2022-06-06 10:14:46 +0700

    update xml dan splash screen

---
+++ app/src/androidTest/java/Splash.kt
@@ -0,0 +1,21 @@
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.traveloka.bestpriceapp.R
+import com.traveloka.bestpriceapp.ui.produk.activity.MainActivity
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@Splash, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
